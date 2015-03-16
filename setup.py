de1866c0c20dcc882b73eefbf2f5f05142d55fa0
tschaefer <github@blackox.org>
2015-03-16 18:49:40 +0100

    director: created setup stuff

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+from setuptools import setup
+
+setup (
+        name='director',
+        version='0.0.1',
+        packages=['director'],
+        install_requires=['Flask >= 0.10.1', 'Flask-SQLAlchemy >= 2.0',
+                          'SQLAlchemy >= 0.9.9'],
+        entry_points={'console_scripts': ['director=director:main']},
+        author='Tobias Schäfer',
+        author_email='director@blackoxorg',
+        url='https://github.com/tschaefer/director',
+        description="Director. Entertainment directed by you.",
+        license='BSD',
+        include_package_data=True
+)
